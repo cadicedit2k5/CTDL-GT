f1b50106d664b7bbf4b70be915df40340048205e
cadicedit2k5 <161547122+cadicedit2k5@users.noreply.github.com>
2024-09-16 10:36:04 +0700

    Update BTH3_1.cpp

--- CTDL_GT/CTDL_GT/BTH3_1.cpp
+++ CTDL_GT/CTDL_GT/BTH3_1.cpp
@@ -73,8 +73,8 @@ struct DSD {
 	void bubbleSort() {
 		for (int i = 0; i < n - 1; i++) {
 			for (int j = n - 1; j > i; j--) {
-				if (a[j] < a[i]) {
-					swap(a[i], a[j]);
+				if (a[j] < a[j - 1]) {
+					swap(a[j - 1], a[j]);
 				}
 			}
 		}
